1e1b7ad67e27641ba19ba137f6cc14a287131d56
Ivan Kamenev <lkamenev92@gmail.com>
2022-04-28 14:20:01 +0300

    initial commit

---
+++ StocksApp/StocksApp/Managers/HapticsManager.swift
@@ -0,0 +1,22 @@
+//
+//  HapticsManager.swift
+//  StocksApp
+//
+//  Created by  Ivan Kamenev on 28.04.2022.
+//
+
+import Foundation
+import UIKit
+final class HapticsManager {
+    static let shared = HapticsManager()
+    
+    private init() {}
+    
+    // MARK: - Public
+    
+    public func vibrateForSelection() {
+        
+    }
+    
+    
+}
